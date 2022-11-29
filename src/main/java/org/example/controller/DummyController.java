ae2134d18e899d5ade47566e1d65dc129df030e2
ranaahmeddd <ronaahmed826@gmail.com>
2022-11-29 21:38:38 +0200

    commit dev

--- src/main/java/org/example/controller/DummyController.java
+++ src/main/java/org/example/controller/DummyController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class DummyController {
     public DummyController(){
+        //dummy
         System.out.println("In DummyController");
     }
 
